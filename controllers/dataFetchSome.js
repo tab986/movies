4e43b5c3b8ecdb6abd1fb2aad09bc31d24392dcd
karrarix <barzankarar@gmail.com>
2026-03-02 14:37:04 +0300

    fix 1 for the day

--- controllers/dataFetchSome.js
+++ controllers/dataFetchSome.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const pool = new Pool({
   user: process.env.POSTGRES_USER,
-  host: process.env.PGHOST,
+  host: process.env.POSTGRES_HOST,
   database: process.env.POSTGRES_DB,
   password: process.env.POSTGRES_PASSWORD,
   port: process.env.PGPORT,
