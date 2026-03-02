7eaa0931619aa0c03d06881a3b6103dbbe675df1
karrarix <barzankarar@gmail.com>
2026-03-03 00:24:40 +0300

    fix 10 for the day

--- routes/liveS.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const app = express();
-const fetchDataSome = require("../controllers/dataFetchSome");
-app.get("/liveS", (req , res) =>{ res = fetchDataSome()});
-
-
-module.exports = router;
