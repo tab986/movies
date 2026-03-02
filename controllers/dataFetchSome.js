6998ae5eaab52de2ad9ea97aa35432f9ac8747b2
karrarix <barzankarar@gmail.com>
2026-03-02 14:41:15 +0300

    fix 2 for the day

--- controllers/dataFetchSome.js
+++ controllers/dataFetchSome.js
@@ -7,6 +7,7 @@ const pool = new Pool({
   user: process.env.POSTGRES_USER,
   host: process.env.POSTGRES_HOST,
   database: process.env.POSTGRES_DB,
+  database: process.env.DATABASE_URL,
   password: process.env.POSTGRES_PASSWORD,
   port: process.env.PGPORT,
   max: 10,
